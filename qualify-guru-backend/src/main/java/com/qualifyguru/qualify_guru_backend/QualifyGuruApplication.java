07c1e43d2b9aaa87e01129911ce951464834f95a
matheus-sortech <matheus.almeida@sortech.com.br>
2026-02-23 19:57:59 -0300

    First Commit

---
+++ qualify-guru-backend/src/main/java/com/qualifyguru/qualify_guru_backend/QualifyGuruApplication.java
@@ -0,0 +1,13 @@
+package com.qualifyguru.qualify_guru_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualifyGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualifyGuruApplication.class, args);
+	}
+
+}
---
+++ qualify-guru-backend/src/test/java/com/qualifyguru/qualify_guru_backend/QualifyGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualifyguru.qualify_guru_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualifyGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
