dc9c183e745c442ac88f8cdcaa81efd85d15aa88
matheus-sortech <matheus.almeida@sortech.com.br>
2026-02-24 20:08:46 -0300

    config: Class to configurate the Spring JPA Data Auditing | Enable JPA Auditing

---
+++ src/main/java/com/qualifyguru/qualify_guru_backend/infrastructure/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.qualifyguru.qualify_guru_backend.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
