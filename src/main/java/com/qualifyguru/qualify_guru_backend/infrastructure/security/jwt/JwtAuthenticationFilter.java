da40d720caae9bb45cf0a28680a7de0e3a306d6d
MTR-S <matheusalmeidass14@gmail.com>
2026-03-03 21:09:04 -0300

    feat: JWT Authentication Filter to deal with the token on the request user header

---
+++ src/main/java/com/qualifyguru/qualify_guru_backend/infrastructure/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.qualifyguru.qualify_guru_backend.infrastructure.security.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        try {
+            userEmail = jwtService.extractUsername(jwt);
+
+            createSpringAuthObject(jwt, userEmail, request);
+        } catch (JwtException | IllegalArgumentException e) {
+
+            SecurityContextHolder.clearContext();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void createSpringAuthObject(String jwt,
+                                        String userEmail,
+                                        HttpServletRequest request) {
+
+        if (jwtService.isTokenValid(jwt)) {
+
+            List<SimpleGrantedAuthority> authorities = jwtService.extractAuthorities(jwt);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userEmail,
+                    null,
+                    authorities
+            );
+
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authToken);}
+    }
+}
