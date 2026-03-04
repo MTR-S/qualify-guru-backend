f71a010e09a8746972c05566e655c43b0245ef6a
MTR-S <matheusalmeidass14@gmail.com>
2026-03-04 19:18:08 -0300

    feat: User Adapte - The implementatioon of the UserRepositoryPort (To fit the Hexagonal Architecture)

---
+++ src/main/java/com/qualifyguru/qualify_guru_backend/infrastructure/adapter/UserPersistenceAdapter.java
@@ -0,0 +1,45 @@
+package com.qualifyguru.qualify_guru_backend.infrastructure.adapter;
+
+import com.qualifyguru.qualify_guru_backend.domain.model.User;
+import com.qualifyguru.qualify_guru_backend.domain.port.out.UserRepositoryPort;
+import com.qualifyguru.qualify_guru_backend.infrastructure.persistence.entity.UserEntity;
+import com.qualifyguru.qualify_guru_backend.infrastructure.persistence.mapper.UserMapper;
+import com.qualifyguru.qualify_guru_backend.infrastructure.persistence.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Component
+public class UserPersistenceAdapter implements UserRepositoryPort {
+
+    private final UserRepository repository;
+    private final UserMapper userMapper;
+
+    public UserPersistenceAdapter(UserRepository repository,
+                                  UserMapper userMapper) {
+        this.repository = repository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    @Transactional
+    public User save(User user) {
+
+        UserEntity entity = repository.findByPublicId(user.getPublicId())
+                .orElseGet(UserEntity::new);
+
+        userMapper.updateEntityFromDomain(user, entity);
+
+        UserEntity savedEntity = repository.save(entity);
+
+        return userMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return repository
+                .findByEmail(email)
+                .map(userMapper::toDomain);
+    }
+}
