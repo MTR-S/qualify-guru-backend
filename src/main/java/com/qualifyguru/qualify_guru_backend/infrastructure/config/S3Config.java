2d6f3e905c737339f87c1d1026646f3bed563439
MTR-S <matheusalmeidass14@gmail.com>
2026-03-25 16:06:09 -0300

    feat: FileStoragePort with uploadAndReturnKey, and config and adapter of FileStoragePort class of S3 AWS Localstack (For dev)

---
+++ src/main/java/com/qualifyguru/qualify_guru_backend/infrastructure/config/S3Config.java
@@ -0,0 +1,35 @@
+package com.qualifyguru.qualify_guru_backend.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+    // Configuration S3 Bucket at localhost usign LocalStack
+
+    @Value("${aws.s3.endpoint:http://localhost:4566}")
+    private String s3Endpoint;
+
+    @Value("${aws.region:us-east-1}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        // No LocalStack, as credenciais podem ser qualquer string não vazia
+                        AwsBasicCredentials.create("test", "test")
+                ))
+                .endpointOverride(URI.create(s3Endpoint))
+                .forcePathStyle(true)
+                .build();
+    }
+}
